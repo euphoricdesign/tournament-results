fcc20ee1f1c2a34955ae3900355e774245eee57c
merlina villecco <noahchukita@gmail.com>
2023-05-31 15:46:02 -0300

    changing footer text

--- src/layout/TournamentLayout.jsx
+++ src/layout/TournamentLayout.jsx
@@ -16,7 +16,7 @@ export const TournamentLayout = ({ children }) => {
 
             { children }
             <div className='text-container'>
-                <p className='small-text'>Este torneo no esta afiliado ni patrocinado por electronic arts ni sus licenciantes. Ver reglamento en kolbi.cr</p>
+                <p className='small-text'>Este torneo no esta afiliado ni patrocinado por Epic Games ni sus licenciantes. Ver reglamento en kolbi.cr</p>
             </div>
        </div>
     )
